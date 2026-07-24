dfcee8af7ffa59d6cdbe59aa7ce8b85c7cee1c37
Eric Krevalis <eric.krevalis@gmail.com>
2026-07-24 20:16:15 +0200

    reposition monitors so main is at [0,0]

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -9,10 +9,11 @@
   # HDMI-A-1 = Zowie XL    (sn GAG02576SL0)    left,   60Hz
   # DP-1     = Zowie XL    (sn EBM2M00765SL0)  middle, 240Hz (main)
   # DP-2     = BenQ RL2455 (sn S4D05178SL0)    right,  60Hz
+  # DP-1 pinned at 0,0, xwayland fullscreen cursor confinement breaks on non-origin outputs
   wayland.windowManager.sway.extraConfig = ''
-    output HDMI-A-1 mode 1920x1080@60Hz position 0,0
-    output DP-1 mode 1920x1080@240Hz position 1920,0
-    output DP-2 mode 1920x1080@60Hz position 3840,0
+    output HDMI-A-1 mode 1920x1080@60Hz position -1920,0
+    output DP-1 mode 1920x1080@240Hz position 0,0
+    output DP-2 mode 1920x1080@60Hz position 1920,0
     workspace 1 output DP-1
     workspace 2 output DP-1
     workspace 3 output DP-1
